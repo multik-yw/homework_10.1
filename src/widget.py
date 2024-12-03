683488ee0c49f2416137ce57aa0fa8da7078057a
Alina Moskvina <alisevenmo@gmail.com>
2024-12-03 23:40:49 +0300

    result_homework_6

---
+++ src/widget.py
@@ -0,0 +1,16 @@
+from src.masks import get_mask_cart_number, get_mask_account
+
+def mask_account_card(number: str) -> str:
+    """Функция маскировки Счета/карты"""
+    if len(number.split()[-1]) == 16:
+        new_cart_number = get_mask_cart_number(number.split()[-1])
+        return f"{number[:16]}{new_cart_number}"
+    elif len(number.split()[-1]) == 20:
+        new_account_number = get_mask_account(number.split()[-1])
+        return f"{number[:20]}{new_account_number}"
+
+
+def get_date(data: str) -> str:
+    """Функция преобразования даты в формате ДД.ММ.ГГГГ"""
+    new_data = data[0:10].split("-")
+    return ".".join(new_data[::-1])
