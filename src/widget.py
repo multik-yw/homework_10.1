797b2c8617fee14c7047f213def4355cceb5e815
Alina Moskvina <alisevenmo@gmail.com>
2024-12-04 00:59:48 +0300

    fixes

--- src/widget.py
+++ src/widget.py
@@ -1,13 +1,15 @@
 from src.masks import get_mask_cart_number, get_mask_account
 
+
 def mask_account_card(number: str) -> str:
     """Функция маскировки Счета/карты"""
     if len(number.split()[-1]) == 16:
         new_cart_number = get_mask_cart_number(number.split()[-1])
-        return f"{number[:16]}{new_cart_number}"
+        result =  f"{number[:16]}{new_cart_number}"
     elif len(number.split()[-1]) == 20:
         new_account_number = get_mask_account(number.split()[-1])
-        return f"{number[:20]}{new_account_number}"
+        result = f"{number[:20]}{new_account_number}"
+    return result
 
 
 def get_date(data: str) -> str:
