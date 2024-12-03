144f62e6dbbce674acd69183e2f7a2f1b17309ad
Alina Moskvina <alisevenmo@gmail.com>
2024-12-03 21:44:05 +0300

    add last homework_5

---
+++ src/masks.py
@@ -0,0 +1,12 @@
+cart_number_str = int()
+account_number_str = int()
+
+
+def get_mask_cart_number(cart_number: str) -> str:
+    """Функция маскировки номера банковской карты"""
+    return f"{cart_number[:4]} {cart_number[4:6]} ** **** {cart_number[-4:]}"
+
+
+def get_mask_account(account_number: str) -> str:
+    """Функция максировки номера банковского счета"""
+    return f"**{account_number[-4:]}"
