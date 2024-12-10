b0e804dc09557122e326a0585d261966bc4e51a8
Alina Moskvina <alisevenmo@gmail.com>
2024-12-10 19:27:39 +0300

    homework_10.1 done

--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,8 @@
-def filters_by_state(list_of_date: list[dict[str, str | int]], value_state: str="EXECUTED") -> list[dict[str, str | int]]:
+def filters_by_state(list_of_date: list[dict[str, str | int]], state: str = "EXECUTED") -> list[dict[str, str | int]]:
     """ Функция фильтрует данные по указанному значению """
     filtered_list = []
     for dictionary in list_of_date:
-        if dictionary.get("state") == value_state:
+        if dictionary.get("state") == state:
             filtered_list.append(dictionary)
     return filtered_list
 
@@ -10,4 +10,4 @@ def filters_by_state(list_of_date: list[dict[str, str | int]], value_state: str=
 def sort_by_data(list_of_date: list, is_reverse: bool = True) -> list:
     """ Функция сортировки списка словарей по дате"""
     sorted_list = sorted(list_of_date, key=lambda x: x["data"], reverse=is_reverse)
-    return  sorted_list
+    return sorted_list
